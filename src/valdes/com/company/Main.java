74132c9a7cc9eb5275726fc7c87cc11dbb49e795
Vladislav Malyshev <58388133+valdesius@users.noreply.github.com>
2022-03-28 21:35:04 +0300

    Update Main.java

--- src/valdes/com/company/Main.java
+++ src/valdes/com/company/Main.java
@@ -1,5 +1,3 @@
-package valdes.com.company;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -9,14 +7,17 @@ public class Main {
         System.out.println("Исходная таблица");
         dataTable.showAllTable();
 
+        //удалить столбец
         dataTable.deleteColumn();
 
         System.out.println("Обновленная");
         dataTable.showAllTable();
 
+        //добавить строку
         dataTable.addRow();
         dataTable.showAllTable();
         System.out.println(dataTable.column("column_name2", 2));
+        
         dataTable.setValue("Hello", 1,1);
 
         System.out.println("Обновленная");
